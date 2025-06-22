43bf3e4727e0b4160a88e1ddbc449a496a298ea0
DaniStrateva <daniystrateva@gmail.com>
2025-06-21 20:00:19 -0600

    db error handling

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -6,7 +6,13 @@ const dbUrl = `mongodb://localhost:27017`;
 //database name depends on the project database
 const dbName = 'test_db';
 
-await mongoose.connect(dbUrl, {dbName});
+    try{
+       await mongoose.connect(dbUrl, {dbName});
+       console.log('DB Connection Successful');
+    }catch(err){
+        console.log('DB Connection Failed');
+        console.log(err.message); 
+    }
 }
 
 export default initDatabase;

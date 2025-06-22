b448f076cf1c44b2ebf1c3de27b96f0dd0419df8
DaniStrateva <daniystrateva@gmail.com>
2025-06-21 21:56:51 -0600

    check if password and repassword match

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,6 +2,10 @@ import User from "../models/User.js"
 
 export default{
     register(userData){
-
+        //check if passwords are the same
+        if(userData.password !== userData.rePassword){
+            throw new Error('Password Missmatch');
+        }
+        return User.create(userData);
     }
 }

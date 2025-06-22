9bd37c0cb3abbeb1ea2f9125c1b8f7d2be940b96
DaniStrateva <daniystrateva@gmail.com>
2025-06-21 22:00:07 -0600

    check if user exists

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,7 +5,14 @@ export default{
         //check if passwords are the same
         if(userData.password !== userData.rePassword){
             throw new Error('Password Missmatch');
+        };
+
+        //check if user exists
+        const user = User.findOne({email: userData.email});
+        if(user){
+            throw new Error('User already exists');
         }
+        
         return User.create(userData);
     }
 }
